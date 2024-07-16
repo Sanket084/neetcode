5a4578fec706c175ac6104d029a915feb599f4fc
Sanket084 <kadlag.sanket82@gmail.com>
2024-07-16 23:02:49 +0530

    Add Stack directory

---
+++ stack/0020-valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { "}" : "{", ")" : "(","]" : "[" }
+        
+        if len(s) % 2 == 1:
+             return False
+
+        for i in s:
+            if i in closeToOpen:
+                if stack and stack[-1] == closeToOpen[i]:
+                    stack.pop()
+                else:
+                    return False
+            else: 
+                stack.append(i)
+        
+        return True if not stack else False
+
