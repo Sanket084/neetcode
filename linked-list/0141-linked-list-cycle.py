f79aeecfc8c73778795c5bb5662ec713a890deff
Sanket084 <kadlag.sanket82@gmail.com>
2024-07-17 22:18:49 +0530

    Update 141 alternate solution

--- linked-list/0141-linked-list-cycle.py
+++ linked-list/0141-linked-list-cycle.py
@@ -4,17 +4,35 @@
 #         self.val = x
 #         self.next = None
 
+# This solution is for the Floyd's Tortoise and Hare (neetcode)
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         
-        curr = head
-        seen = []
+        slow, fast = head, head
 
-        while curr:
-            if curr not in seen:
-                seen.append(curr)
-                curr = curr.next
-            elif curr in seen:
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
                 return True
         return False
+    
+
+
+# Below is the solution I came up with
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+#         curr = head
+#         seen = []
+
+#         while curr:
+#             if curr not in seen:
+#                 seen.append(curr)
+#                 curr = curr.next
+#             elif curr in seen:
+#                 return True
+#         return False
         
