31417c5688d2202b140cf6cf75745de3e3003954
Sanket084 <kadlag.sanket82@gmail.com>
2024-08-12 13:58:29 +0530

    Update 11. Container with Most Water

--- two-pointers/0011-container-with-most-water.py
+++ two-pointers/0011-container-with-most-water.py
@@ -1,13 +1,25 @@
 class Solution:
-    def maxArea(self, heights: List[int]) -> int:
-        l, r = 0, len(heights) - 1
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
         res = 0
 
         while l < r:
-            res = max(res, min(heights[l], heights[r]) * (r - l))
-            if heights[l] < heights[r]:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
                 l += 1
-            elif heights[r] <= heights[l]:
+            elif height[r] <= height[l]:
                 r -= 1
             
         return res
+
+
+# Given below is the Brute Force solution which throws Time Limit Exceeded error.
+
+# class Solution:
+#     def maxArea(self, height: List[int]) -> int:
+#         res = 0
+
+#         for L in range(len(height)):
+#             for R in range(l + 1, len(height)):
+#                 area = (R - L) * min(height[L], height[R])
+#                 res = max(res, area)
